ab2067008f21ecba9611d0f5a712c1fbf0694065
shruti hanchate <shrutihanchate98@gmail.com>
2021-03-08 20:24:24 +0530

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/book-frontend'),
+      dir: require('path').join(__dirname, './coverage/bookfrontend'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-frontend'`, () => {
+  it(`should have as title 'bookfrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-frontend');
+    expect(app.title).toEqual('bookfrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('book-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bookfrontend app is running!');
   });
 });
